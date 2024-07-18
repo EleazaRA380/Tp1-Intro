7aedaffcc4189645b961f8abb97a8737ed6f6fbe
SofiLBZ <sbica@fi.uba.ar>
2024-07-18 13:27:17 -0300

    actualización de datos de las tablas

--- base de datos/archivo.sql
+++ base de datos/archivo.sql
@@ -1,6 +1,4 @@
---
 -- PostgreSQL database dump
---
 
 -- Dumped from database version 15.7 (Ubuntu 15.7-0ubuntu0.23.10.1)
 -- Dumped by pg_dump version 15.7 (Ubuntu 15.7-0ubuntu0.23.10.1)
@@ -21,6 +19,19 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: usuarios; Type: TABLE; Schema: public; Owner: usuario
+--
+
+CREATE TABLE public.usuarios (
+    id integer NOT NULL,
+    usuario character varying(255) NOT NULL,
+    contrasena character varying(20) NOT NULL,
+    personalizaciones_id integer
+);
+
+ALTER TABLE public.usuarios OWNER TO usuario;
+
+--
 -- Name: personalizaciones; Type: TABLE; Schema: public; Owner: usuario
 --
 
@@ -29,11 +40,9 @@ CREATE TABLE public.personalizaciones (
     viento boolean,
     lluvia boolean,
     humedad boolean,
-    "presionAtmosferica" boolean,
-    "sensacionTermica" boolean,
-    usuario_id integer
+    temperatura boolean,
+    sensacionTermica boolean
 );
-
 
 ALTER TABLE public.personalizaciones OWNER TO usuario;
 
@@ -42,87 +51,26 @@ ALTER TABLE public.personalizaciones OWNER TO usuario;
 --
 
 CREATE SEQUENCE public.personalizaciones_id_seq
-    AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 
-
 ALTER TABLE public.personalizaciones_id_seq OWNER TO usuario;
-
---
--- Name: personalizaciones_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: usuario
---
-
-ALTER SEQUENCE public.personalizaciones_id_seq OWNED BY public.personalizaciones.id;
-
-
---
--- Name: usuarios; Type: TABLE; Schema: public; Owner: usuario
---
-
-CREATE TABLE public.usuarios (
-    id integer NOT NULL,
-    usuario character varying(255) NOT NULL,
-    contrasena character varying(20) NOT NULL
-);
-
-
-ALTER TABLE public.usuarios OWNER TO usuario;
 
 --
 -- Name: usuarios_id_seq; Type: SEQUENCE; Schema: public; Owner: usuario
 --
 
 CREATE SEQUENCE public.usuarios_id_seq
-    AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 
-
 ALTER TABLE public.usuarios_id_seq OWNER TO usuario;
-
---
--- Name: usuarios_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: usuario
---
-
-ALTER SEQUENCE public.usuarios_id_seq OWNED BY public.usuarios.id;
-
-
---
--- Name: personalizaciones id; Type: DEFAULT; Schema: public; Owner: usuario
---
-
-ALTER TABLE ONLY public.personalizaciones ALTER COLUMN id SET DEFAULT nextval('public.personalizaciones_id_seq'::regclass);
-
-
---
--- Name: usuarios id; Type: DEFAULT; Schema: public; Owner: usuario
---
-
-ALTER TABLE ONLY public.usuarios ALTER COLUMN id SET DEFAULT nextval('public.usuarios_id_seq'::regclass);
-
-
---
--- Data for Name: personalizaciones; Type: TABLE DATA; Schema: public; Owner: usuario
---
-
-COPY public.personalizaciones (id, viento, lluvia, humedad, "presionAtmosferica", "sensacionTermica", usuario_id) FROM stdin;
-\.
-
-
---
--- Data for Name: usuarios; Type: TABLE DATA; Schema: public; Owner: usuario
---
-
-COPY public.usuarios (id, usuario, contrasena) FROM stdin;
-\.
-
 
 --
 -- Name: personalizaciones_id_seq; Type: SEQUENCE SET; Schema: public; Owner: usuario
@@ -130,39 +78,26 @@ COPY public.usuarios (id, usuario, contrasena) FROM stdin;
 
 SELECT pg_catalog.setval('public.personalizaciones_id_seq', 1, false);
 
-
 --
 -- Name: usuarios_id_seq; Type: SEQUENCE SET; Schema: public; Owner: usuario
 --
 
 SELECT pg_catalog.setval('public.usuarios_id_seq', 1, false);
 
-
 --
--- Name: personalizaciones personalizaciones_pkey; Type: CONSTRAINT; Schema: public; Owner: usuario
+-- Name: personalizaciones_pkey; Type: CONSTRAINT; Schema: public; Owner: usuario
 --
 
 ALTER TABLE ONLY public.personalizaciones
     ADD CONSTRAINT personalizaciones_pkey PRIMARY KEY (id);
 
-
 --
--- Name: usuarios usuarios_pkey; Type: CONSTRAINT; Schema: public; Owner: usuario
+-- Name: usuarios_pkey; Type: CONSTRAINT; Schema: public; Owner: usuario
 --
 
 ALTER TABLE ONLY public.usuarios
     ADD CONSTRAINT usuarios_pkey PRIMARY KEY (id);
 
-
 --
--- Name: personalizaciones personalizaciones_usuario_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: usuario
+-- End of dump
 --
-
-ALTER TABLE ONLY public.personalizaciones
-    ADD CONSTRAINT personalizaciones_usuario_id_fkey FOREIGN KEY (usuario_id) REFERENCES public.usuarios(id);
-
-
---
--- PostgreSQL database dump complete
---
-
