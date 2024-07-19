82add467e055b350f6f7fdb4ee1b16803eb07ca3
Eleazar <esandovalh@fi.uba.ar>
2024-07-19 17:31:09 -0300

    Arreglo: Codigo de la aplicacion del clima

--- backend/weather.py
+++ backend/weather.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from urllib.parse import unquote #Replace %xx escapes by their single-character equivalent.
+from urllib.parse import unquote # Funcion Unquote reemplaza %xx escapes con su equivalente de un solo carácter (vacio("")).
 import json
 import requests
 app = Flask(__name__)
@@ -10,51 +10,72 @@ def get_weather():
     url_geolocalizator="http://api.openweathermap.org/geo/1.0/direct"
     city_name=unquote(request.json.get("city"))
     api_key="b7befde37d3eb977bb9a8f48e7de1c75"
-    limit=5
+    limit=1
     location=f"{city_name}"
+    state_name=None
+    iso_state_code=None
     if("state" in request.json):
-        us_x=False
         state_name=unquote(request.json.get("state"))     
         with open("us_states_ISO2.json", "r") as us_data_2:
             states_file_2=json.load(us_data_2)
             for indice in range(len(states_file_2)):
                 if((states_file_2[indice]["state_en"]==state_name) or (states_file_2[indice]["state_es"]==state_name)): #Si el valor de la clave nameEs o nameEN  en el indice X es igual "country", entonces.
                     iso_state_code=states_file_2[indice]["iso"]
-                    location=f"{city_name},{iso_state_code}" 
-                    us_x=True    
-        if(us_x!=True):
-            location=f"{city_name},{state_name}"
+                    location=f"{city_name},{iso_state_code}"   
+                else:
+                    location=f"{city_name},{state_name}"
     if("country" in request.json): 
-        country_name=unquote(request.json.get("country")) #guardas el nombre del pais.
-        with open("countries_ISO.json", "r") as data_country:  #Abrimos el archivo .json en modo lectura
-            countries_file=json.load(data_country) #guardamos el archivo. 
-            for indice in range(len(countries_file)): #Iteramos para elemento de la lista del archivo "countries_ISO.json".
-                if((countries_file[indice]["nameES"]==country_name) or (countries_file[indice]["nameEN"]==country_name)): #Si el valor de la clave nameEs o nameEN  en el indice X es igual "country", entonces.
-                    iso_country_code=countries_file[indice]["iso2"] #guardamos el valor de la clave "iso2" (codigo del pais ISO) encontrado en el indice X. 
-                    location=f"{city_name},{state_name},{iso_country_code}"
-    geografic_structure={
+        country_name=unquote(request.json.get("country"))   #guardas el nombre del pais.
+        with open("countries_ISO.json", "r") as data_country:   #Abrimos el archivo .json en modo lectura
+            countries_file=json.load(data_country)  #guardamos el archivo. 
+            for indice in range(len(countries_file)):   #Iteramos para elemento de la lista del archivo "countries_ISO.json".
+                if((countries_file[indice]["nameES"]==country_name) or (countries_file[indice]["nameEN"]==country_name)):   #Si el valor de la clave nameEs o nameEN  en el indice X es igual "country", entonces.
+                    iso_country_code=countries_file[indice]["iso2"]     #guardamos el valor de la clave "iso2" (codigo del pais ISO) encontrado en el indice X. 
+                    if(iso_state_code==None):
+                        location=f"{city_name},{state_name},{iso_country_code}"
+                    else:
+                        location=f"{city_name},{iso_state_code},{iso_country_code}"
+    geografic_params={
         "q":location,
         "limit":limit,
         "appid":api_key
     }
-    response=requests.get(url_geolocalizator,params=geografic_structure)
-    response_coordinates=response.json()
-    retorno=[]
-    if(response.status_code==200):
-        for indice in range(len(response_coordinates)):
-            lat=response_coordinates[indice]["lat"]
-            lon=response_coordinates[indice]["lon"]
-            weather={
-                "lat":lat,
-                "lon":lon,
-                "appid":api_key,
-                "units":"metric"
-            }
-            response_weather=requests.get(url_weather, params=weather)
-            retorno.append(response_weather.json())
+    response_coordinates=requests.get(url_geolocalizator,params=geografic_params)
+    coordinates=response_coordinates.json()
+    weather=[]
+    if(response_coordinates.status_code==200 and len(coordinates)>0):
+        lat=coordinates[0]["lat"]
+        lon=coordinates[0]["lon"]
+        country=coordinates[0]["country"]
+        state=None
+        if("state" in coordinates[0]):
+            state=coordinates[0]["state"]
+        weather_params={
+            "lat":lat,
+            "lon":lon,
+            "appid":api_key,
+            "units":"metric"
+        }
+        response_weather=requests.get(url_weather, params=weather_params).json()
+        weather.append(response_weather)
+        with open("countries_ISO.json") as data_country:
+            countries_file=json.load(data_country) #guardamos el archivo. 
+            for indice in range(len(countries_file)):
+                if(countries_file[indice]["iso2"]==country): #Si el valor de la clave "iso2" es igual a country. 
+                    country_ES=countries_file[indice]["nameES"]
+                    country_EN=countries_file[indice]["nameEN"]
+                    if(state==None):
+                        new_dictionary={"country_ES":country_ES,"country_EN":country_EN}
+                        weather.append(new_dictionary)
+                    else:
+                        new_dictionary={"state":state,"country_ES":country_ES,"country_EN":country_EN}
+                        weather.append(new_dictionary)
+    elif not coordinates:
+        new_dictionary={"location":"Not Found","Valid":"False"}
+        weather.append(new_dictionary)
     else:
-        retorno.append(response.json())
-    return jsonify(retorno)
+        weather.append(coordinates)
+    return jsonify(weather)
 
 if __name__ == '__main__':
     app.run(debug=True)
