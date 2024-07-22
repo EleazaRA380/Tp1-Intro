4536865c0f51b822732eeb4d3af3139400069816
SofiLBZ <sbica@fi.uba.ar>
2024-07-22 12:49:21 -0300

    arreglo de la validacion de datos para registrar al usuario

--- static/auth.js
+++ static/auth.js
@@ -1,62 +1,55 @@
-function verifyRegister() {
-  const password=document.getElementById("userPass").value;
-  const confirm_password=document.getElementById("repeatUserPass").value;
-  const error_element=document.getElementById("errorMessage");
-  const user=document.getElementById("userName").value;
-  var itsvalid=false;
-  if(user.length<=18){
-    if(password.length<=10){
-      if (password === confirm_password){
-        itsvalid=true; // Las contraseñas coinciden"
-        
-      }   
-      else{
-        error_element.textContent="passwords are not the same."; 
-        // Las contraseñas no coinciden
-      }
-    }
-    else{ 
-      error_element.textContent="The password cannot be longer than 10 characters.";
-    }
-  }
-  else{
-    error_element.textContent="The username cannot be longer than 18 characters.";
-  }
-
-  return itsvalid;
-}
-
+//validacion de los campos de register y creación de un usuario
 function registerUsuario() {
-  var userName = document.getElementById("userName").value;
-  var userPass = document.getElementById("userPass").value;
-  var repeatUserPass = document.getElementById("repeatUserPass").value;
+  let userName = document.getElementById("userName").value;
+  let userPass = document.getElementById("userPass").value;
+  let repeatUserPass = document.getElementById("repeatUserPass").value;
 
   if (userPass !== repeatUserPass) {
-      document.getElementById("errorMessage").innerText = "Passwords do not match";
+      document.getElementById("errorMessage").innerText = "Las contraseñas no coinciden";
       return false;
   }
 
-  var formData = {
+  if (userPass.length < 4 || userPass.length > 18) {
+      document.getElementById("errorMessage").innerText = "La contraseña debe tener entre 4 y 18 caracteres";
+      return false;
+  }
+
+  if (userName.length < 4 || userName.length > 18) {
+      document.getElementById("errorMessage").innerText = "El nombre de usuario debe tener entre 4 y 18 caracteres";
+      return false;
+  }
+  let formData = {
       userName: userName,
       userPass: userPass
   };
-
-  fetch('/usuarios/register', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-  })
-  .then(response => response.json())
-  .then(data => {
-      console.log('Success:', data);
-      window.location.href = "/login";
-  })
-  .catch(error => {
-      console.error('Error:', error);
-      
-  });
+  fetch('/check_username/' + userName)
+      .then(response => response.json())
+      .then(data => {
+          if (data.message === 'El nombre de usuario está disponible') {
+              fetch('/usuarios/register', {
+                  method: 'POST',
+                  headers: {
+                      'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(formData)
+              })
+              .then(response => response.json())
+              .then(data => {
+                  console.log('Success:', data);
+                  window.location.href = "/login"; 
+              })
+              .catch(error => {
+                  console.error('Error al registrar usuario:', error);
+                
+              });
+          } else {
+              document.getElementById("errorMessage").innerText = "El nombre de usuario no esta disponible";
+          }
+      })
+      .catch(error => {
+          console.error('Error al verificar nombre de usuario:', error);
+          document.getElementById("usernameAvailabilityMessage").innerText = 'Error al verificar nombre de usuario';
+      });
 
   return false; 
 }
